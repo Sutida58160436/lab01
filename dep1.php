fb5540fce2c3a9aaea93918055437c7b3822fa1a
Sutida58160436 <58160436@go.buu.ac.th>
2017-01-24 15:17:59 +0700

    add init

---
+++ dep1.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<title>หน้าแรก</title>
+		<META CHARSET="UTF-8">
+	</head>
+	<body ALIGN="center">
+		<table width="100%" border="0">
+			<tr>
+				<marquee direction="left" bgcolor="#FFFF99"><FONT FACE="JasmineUPC" SIZE=5 color="#696969"><B>ยินดีต้อนรับ </B></FONT></marquee>
+			</tr>	
+			<tr>
+				<th BACKGROUND="cc.png" WIDTH=1015 HEIGHT=55><FONT FACE="JasmineUPC" SIZE=7 ><B>ตัวอย่างหน้าเว็บไซต์</B></FONT></th>
+			</tr>
+			<tr valign="top" height="450">
+				<td BACKGROUND="cc.png" ALIGN="center">
+					<img src="frame.jpg" WIDTH=648 HEIGHT=480>
+				</td>
+			</tr> <br /><br />
+
+			<font face="JasmineUPC" size="+3">
+    		<font color="blue">ในฐานะที่ฉันเป็น</font>
+    		<font color="magenta">ผู้หญิงที่ชอบในความสวยงาม</font> <br>
+   	        <font color="blue">ฉันต้องการ</font>
+   			<font color="magenta">ทำเว็บไซต์เกี่ยวกับความสวยงามของผู้หญิง</font> <br>
+            <font color="blue"> เพื่อที่จะ</font>
+            <font color="magenta">เป็นประโยชน์แก่ผู้ที่เข้าชมเว็บไซต์</font>
+            <br><br>
+		</table>
+	</body>
+</html>
---
+++ dep2.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>หน้าแรก</title>
+		<META CHARSET="UTF-8">
+	</head>
+	<body>
+		<table width="100%" border="0">
+			<tr>
+				<marquee direction="left" bgcolor="#FFFF99"><FONT FACE="JasmineUPC" SIZE=5 color="#696969"><B>ยินดีต้อนรับ </B></FONT></marquee>
+			</tr>	
+			<tr>
+				<th BACKGROUND="cc.png" WIDTH=1015 HEIGHT=55><FONT FACE="JasmineUPC" SIZE=7 ><B>ส่วนเนื้อหา</B></FONT></th>
+			</tr>
+			<tr valign="top" height="450">
+				<td>
+					<img src="cc.png" WIDTH=1015 HEIGHT=450>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
